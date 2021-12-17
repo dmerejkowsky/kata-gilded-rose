5be534acee5815ed9f05568b78778e7b04e3aca1
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 15:56:51 +0100

    Better starting point

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -8,6 +8,31 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
+def main():
+    items = [
+        Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+        Item(name="Aged Brie", sell_in=2, quality=0),
+        Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        Item(name="Conjured Mana Cake", sell_in=3, quality=6),
+    ]
+
+    gilded_rose = GildedRose(items)
+
+    days = 30
+    for day in range(days):
+        print("-------- day %s --------" % day)
+        print("name, sellIn, quality")
+        for item in items:
+            print(item)
+        print("")
+        gilded_rose.update_quality()
+
+
 class GildedRose(object):
     def __init__(self, items):
         self.items = items
@@ -15,7 +40,6 @@ class GildedRose(object):
     # /!\ Do not change code above this line /!\ #
 
     def update_quality(self):
-        raise Exception("kaboom")
         for item in self.items:
             if (
                 item.name != "Aged Brie"
@@ -47,3 +71,7 @@ class GildedRose(object):
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
+
+
+if __name__ == "__main__":
+    main()
