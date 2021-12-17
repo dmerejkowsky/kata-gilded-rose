5cc996f53fc6d808e864ce49b93f54822c4a3015
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-17 21:39:36 +0100

    Make the test pass

--- javascript/test/test_gilded_rose.js
+++ javascript/test/test_gilded_rose.js
@@ -2,10 +2,10 @@ var {expect} = require('chai');
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+  it("update quality of a normal item", function() {
+    const gildedRose = new Shop([ new Item("foo", 20, 10) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("fixme");
+    expect(items[0].quality).to.equal(9);
   });
 
 });
