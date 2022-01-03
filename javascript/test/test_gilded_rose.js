c91d23bbcd1ce4b8f1cf6de9e80fc7043a020aaa
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-01-03 16:38:05 +0100

    Better javascript tests

--- javascript/test/test_gilded_rose.js
+++ javascript/test/test_gilded_rose.js
@@ -3,9 +3,13 @@ var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
   it("update quality of a normal item", function() {
-    const gildedRose = new Shop([ new Item("foo", 20, 10) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.equal(9);
+    const foo = new Item("foo", 20, 10);
+
+    const shop = new Shop([foo]);
+    const items = shop.updateQuality();
+
+    expect(foo.sellIn).to.equal(19);
+    expect(foo.quality).to.equal(9);
   });
 
 });
