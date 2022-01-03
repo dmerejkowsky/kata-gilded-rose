75e3a8213e27385312feb3d6aae7fff0b83f2433
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-01-03 16:53:27 +0100

    python: better Item.__repr__()

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -5,6 +5,9 @@ class Item:
         self.quality = quality
 
     def __repr__(self):
+        return f"Item(name={self.name} sell_in={self.sell_in}, quality={self.quality})"
+
+    def __str__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
