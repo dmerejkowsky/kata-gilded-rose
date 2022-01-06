91a9ccefc5ed5db37c36f4d6f347fd7e4b125920
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-01-06 16:49:17 +0100

    cs: Move Main() code into GildedRoseTests.goldenMaster()

--- cs/GildedRoseTests/GildedRoseTest.cs
+++ cs/GildedRoseTests/GildedRoseTest.cs
@@ -1,6 +1,8 @@
 ﻿using Xunit;
 using System.Collections.Generic;
 using GildedRoseKata;
+using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace GildedRoseTests
 {
@@ -17,5 +19,72 @@ namespace GildedRoseTests
             Assert.Equal(19, defaultItem.SellIn);
             Assert.Equal(9, defaultItem.Quality);
         }
+
+        [Fact]
+        public void goldenMaster() {
+            IList<Item> items = new List<Item>{
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49
+                },
+				// this conjured item does not work properly yet
+				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+
+            var app = new GildedRose(items);
+            IList<string> actualLog = new List<string>();
+
+            for (var i = 0; i < 31; i++)
+            {
+                actualLog.Add("-------- day " + i + " --------");
+                actualLog.Add("name, sellIn, quality");
+                for (var j = 0; j < items.Count; j++)
+                {
+                    actualLog.Add(items[j].Name + ", " + items[j].SellIn + ", " + items[j].Quality);
+                }
+                app.UpdateQuality();
+            }
+
+            string thisPath = GetThisFilePath();
+            string thisDirectory = Path.GetDirectoryName(thisPath);
+
+
+            IList<string> expectedLog = new List<string>();
+            string expectedLogPath = thisDirectory + "/../../golden-master/expected-output.txt";
+            foreach (string line in File.ReadLines(expectedLogPath)) {
+                if(line.Length != 0) {
+                    expectedLog.Add(line);
+                }
+            }
+
+            Assert.Equal(expectedLog, actualLog);
+
+        }
+
+        private static string GetThisFilePath([CallerFilePath] string path = null)
+        {
+            return path;
+        }
+    
     }
 }
+
